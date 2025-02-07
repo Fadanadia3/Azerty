bf323b8e15487186383937d914985d8d6087d1a4
Fadanadia3 <fadanadia3@gmail.com>
2025-02-07 14:31:42 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,29 @@
 import { AppProps } from 'next/app';
-import { WagmiConfig, createConfig } from 'wagmi';
-import { RainbowKitProvider, darkTheme, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { WagmiConfig, createClient, configureChains } from 'wagmi';
+import { RainbowKitProvider, getDefaultWallets, darkTheme } from '@rainbow-me/rainbowkit';
 import { mainnet, polygon } from 'wagmi/chains';
-import { configureChains } from 'wagmi';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'; // Correct import for providers
+import { publicProvider } from 'wagmi/providers/public';
 
-
-
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
-if (!projectId) {
-  throw new Error('NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID est manquant dans les variables d’environnement.');
-}
-
-// Instead of publicProvider, you can use a jsonRpcProvider or others
-// If you want to use publicProvider, you have to install @wagmi/core
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon],
-  [jsonRpcProvider()] // Or use other providers like infuraProvider, alchemyProvider, etc.
-);
+// Configuration de Wagmi pour gérer les chaînes et les connecteurs
+const { chains, provider } = configureChains([mainnet, polygon], [
+  publicProvider(),
+]);
 
 const { connectors } = getDefaultWallets({
-  appName: 'drainerweb',
-  projectId,
-  chains
+  appName: 'MyWeb3App',
+  chains,
 });
 
-const config = createConfig({
+const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  publicClient,
+  provider,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig config={config}>
-      <RainbowKitProvider theme={darkTheme()} modalSize="compact" chains={chains}>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
