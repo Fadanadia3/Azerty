5af575c2ebdf4ea44f2349551f2e026d59d450f6
Fadanadia3 <fadanadia3@gmail.com>
2025-02-07 03:13:15 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAccount, useConnect, useSigner } from 'wagmi'; // Remplacé useProvider par useSigner
+import { useAccount, useConnect, useProvider } from 'wagmi'; // Supprimer useSigner
 import { WalletConnectConnector } from '@wagmi/core';
 import { ethers } from 'ethers';
 
@@ -98,7 +98,7 @@ export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
   const { isConnected: accountConnected, address } = useAccount();
   const { connect, disconnect } = useConnect();
-  const { data: signer } = useSigner(); // Utilisation de useSigner pour obtenir le signer
+  const provider = useProvider(); // Utilisation du provider à la place de useSigner pour ethers v6
 
   useEffect(() => {
     if (accountConnected) {
@@ -108,7 +108,8 @@ export default function Home() {
   }, [accountConnected]);
 
   const interactWithContract = async () => {
-    if (signer && address) {
+    if (provider && address) {
+      const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
       try {
